7842fb03f713435f199850c3aecc2393f430ccc8
KAUSTAVCAPGEMINIAJ-17 <83285047+KAUSTAVCAPGEMINIAJ-17@users.noreply.github.com>
2021-05-19 18:11:50 +0530

    Add files via upload

---
+++ Day 4 Case Study/CASE STUDY 1/Student.java
@@ -0,0 +1,35 @@
+package casestudy;
+
+public class Student extends Person {
+	
+		private String program;
+		private int year;
+		private double fees;
+		
+		public String getProgram() {
+			return program;
+		}
+		public void setProgram(String program) {
+			this.program = program;
+		}
+		public int getYear() {
+			return year;
+		}
+		public void setYear(int year) {
+			this.year = year;
+		}
+		public double getFees() {
+			return fees;
+		}
+		public void setFees(double fees) {
+			this.fees = fees;
+		}
+		
+		public String toString() {
+			return " program: "+
+		this.program+", Year: "+this.year+", fees: "+this.fees;
+		}
+		
+	}
+
+
